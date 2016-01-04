f849336ee38d23f6a6a7e5da525245949945fa63
glenda@enzo <glenda@enzo>
2016-01-04 21:37:32 -0200

    almost finished

--- main.c
+++ main.c
@@ -4,26 +4,78 @@
 #include <thread.h>
 #include <9p.h>
 
+#define DBG if(debug) print
+
 static char Enotimpl[] = "dchan: not implemented";
 static char Enotperm[] = "dchan: operation not permitted";
+static char Eintern[] = "dchan: internal error";
+
+typedef struct Data Data;
+
+struct Data {
+	char *val;
+	int valsz;
+};
 
 enum
 {
 	Xctl = 1,
 };
 
+Channel *chan;
+Reqqueue *rqueue, *wqueue;
+int debug;
+
+void
+syncread(Req *r)
+{
+	Data d;
+	DBG("fsread: Count = %d\n", r->ifcall.count);
+
+	int err = recv(chan, &d);
+
+	DBG("Recv: %d\n", err);
+	readbuf(r, d.val, d.valsz);
+	
+	free(d.val);
+	free(&d);
+
+	DBG("fsread: Reporting a success read: (%s):%d\n", r->ofcall.data, r->ofcall.count);
+	respond(r, nil);
+}
+
 void
 fsread(Req *r)
 {
-	char msg[] = "hacked by i4k";
-	readstr(r, msg);
+	reqqueuepush(rqueue, r, syncread);
+}
+
+void syncwrite(Req *r)
+{
+	Data *d;
+	DBG("fswrite: Count = %d\n", r->ifcall.count);
+
+	d = mallocz(sizeof *d, 1);
+
+	if(!d)
+		respond(r, Eintern);
+
+	d->val = strdup(r->ifcall.data);
+	d->valsz = r->ifcall.count;
+	int err = send(chan, d);
+
+	DBG("Send: %d\n", err);
+
+	r->ofcall.count = d->valsz;
+
+	DBG("fswrite: Reporting success write: %s\n", d->val);
 	respond(r, nil);
 }
 
 void
 fswrite(Req *r)
 {
-	respond(r, Enotimpl);
+	reqqueuepush(wqueue, r, syncwrite);
 }
 
 void 
@@ -38,10 +90,21 @@ fsopen(Req *r)
 	respond(r, nil);
 }
 
+void fsend(Srv *)
+{
+	chanfree(chan);
+}
+
 void
 fsdestroyfile(File *f)
 {
 	closefile(f);
+}
+
+void usage(void)
+{
+	fprint(2, "Usage: dchan [-D] [-s srvname] [-m mptp]\n");
+	threadexits("usage");
 }
 
 Srv fs = {
@@ -49,20 +112,19 @@ Srv fs = {
 	.read 	= fsread,
 	.write 	= fswrite,
 	.create	= fscreate,
+	.end = fsend,
 };
 
-void usage(void)
-{
-	fprint(2, "Usage: dchan [-D] [-s srvname] [-m mptp]\n");
-	exits("usage");
-}
-
-
-void main(int argc, char *argv[])
+void
+threadmain(int argc, char *argv[])
 {
 	char *addr = nil;
 	char *srvname = nil;
 	char *mptp = nil;
+	
+	chan = chancreate(sizeof(struct Data), 0);
+	rqueue = reqqueuecreate();
+	wqueue = reqqueuecreate();
 
 	Qid q;
 
@@ -72,10 +134,12 @@ void main(int argc, char *argv[])
 	createfile(fs.tree->root, "ctl", "ctl", 0666, (void*)Xctl);
 
 	ARGBEGIN {
+	case 'd':
+		debug++;
+		break;
 	case 'D':
 		chatty9p++;
 		break;
-
 	case 'a':
 		addr = EARGF(usage());
 		break;
@@ -101,12 +165,12 @@ void main(int argc, char *argv[])
 	}
 
 	if(addr) {
-		listensrv(&fs, addr);
+		threadlistensrv(&fs, addr);
 	}
 
 	if(srvname || mptp) {
-		postmountsrv(&fs, srvname, mptp, MREPL|MCREATE);
+		threadpostmountsrv(&fs, srvname, mptp, MREPL|MCREATE);
 	}
 
-	exits(0);
+	threadexits(0);
 }
